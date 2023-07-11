10af05c734fea02d4b46337d002759cb5ead6abb
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2023-07-11 22:09:43 +0100

    Fix JSX/HTML errors

--- app/persona/[id]/page.tsx
+++ app/persona/[id]/page.tsx
@@ -1,8 +1,8 @@
-"use client";
-import { Avatar } from "flowbite-react";
-import { Fragment, useEffect, useState } from "react";
-import Header from "../../components/navbar";
-import { Persona } from "../../types";
+'use client';
+import { Avatar } from 'flowbite-react';
+import { Fragment, useEffect, useState } from 'react';
+import Header from '../../components/navbar';
+import { Persona } from '../../types';
 
 // async function getPersona(personaId: number) {
 //   const res = await fetch("http://localhost:3001/personas", {
@@ -17,7 +17,7 @@ export default function PersonaPage({ params }: any): JSX.Element {
   const [notFound, setNotFound] = useState<Boolean>(false);
 
   useEffect(() => {
-    fetch("http://localhost:3001/personas")
+    fetch('http://localhost:3001/personas')
       .then((res) => res.json())
       .then((data) => {
         let output = data[params.id - 1];
@@ -59,16 +59,16 @@ export default function PersonaPage({ params }: any): JSX.Element {
                   accusamus!
                 </p>
                 <form className="mt-4">
-                  <label for="chat" class="sr-only">
+                  <label htmlFor="chat" className="sr-only">
                     Your message
                   </label>
-                  <div class="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
+                  <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
                     <button
                       type="button"
-                      class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+                      className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
                     >
                       <svg
-                        class="w-5 h-5"
+                        className="w-5 h-5"
                         aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -93,14 +93,14 @@ export default function PersonaPage({ params }: any): JSX.Element {
                           d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"
                         />
                       </svg>
-                      <span class="sr-only">Upload image</span>
+                      <span className="sr-only">Upload image</span>
                     </button>
                     <button
                       type="button"
-                      class="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+                      className="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
                     >
                       <svg
-                        class="w-5 h-5"
+                        className="w-5 h-5"
                         aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -114,20 +114,20 @@ export default function PersonaPage({ params }: any): JSX.Element {
                           d="M13.408 7.5h.01m-6.876 0h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM4.6 11a5.5 5.5 0 0 0 10.81 0H4.6Z"
                         />
                       </svg>
-                      <span class="sr-only">Add emoji</span>
+                      <span className="sr-only">Add emoji</span>
                     </button>
                     <textarea
                       id="chat"
-                      rows="1"
-                      class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      // rows="1"
+                      className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Your message..."
                     ></textarea>
                     <button
                       type="submit"
-                      class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
+                      className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
                     >
                       <svg
-                        class="w-5 h-5 rotate-90"
+                        className="w-5 h-5 rotate-90"
                         aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="currentColor"
@@ -135,7 +135,7 @@ export default function PersonaPage({ params }: any): JSX.Element {
                       >
                         <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
                       </svg>
-                      <span class="sr-only">Send message</span>
+                      <span className="sr-only">Send message</span>
                     </button>
                   </div>
                 </form>
