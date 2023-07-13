9176187d8aa15e73e18f19a88bc1bf5b53c00917
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2023-07-13 16:59:48 +0100

    conditional rendering for error message
    Added state to determine if there is an error on the page or not

--- app/page.tsx
+++ app/page.tsx
@@ -6,31 +6,58 @@ import PersonaCard from './components/persona-card/persona-card';
 import { Area, Persona } from './types';
 
 export default function Index(): JSX.Element {
+  const [areas, setAreas] = useState<Area[]>([]);
+  const [personas, setPersonas] = useState<Persona[]>([]);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/areas')
+      .then((response) => response.json())
+      .then((data) => {
+        setAreas(data);
+        setIsError(false);
+      })
+      .catch((error: Error) => {
+        console.log(error);
+        setIsError(true);
+      });
+    fetch('http://localhost:3001/personas')
+      .then((res) => res.json())
+      .then((data) => {
+        setPersonas(data);
+        setIsError(false);
+      })
+      .catch((error: Error) => {
+        console.log(error);
+        setIsError(true);
+      });
+  }, []);
+
   return (
     <Fragment>
       <Header />
       <div className="flex dark:bg-gray-900">
         <main className="order-2 mx-4 mt-4 mb-24 flex-[1_0_16rem]">
-          <HomePage />
+          {isError ? (
+            <p className="text-white">
+              An error occurred. Please try again later.
+            </p>
+          ) : (
+            <HomePage areas={areas} personas={personas} />
+          )}
         </main>
       </div>
     </Fragment>
   );
 }
 
-function HomePage(): JSX.Element {
-  const [areas, setAreas] = useState<Area[]>([]);
-  const [personas, setPersonas] = useState<Persona[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3001/areas')
-      .then((response) => response.json())
-      .then((data) => setAreas(data));
-    fetch('http://localhost:3001/personas')
-      .then((res) => res.json())
-      .then((data) => setPersonas(data));
-  }, []);
-
+function HomePage({
+  areas,
+  personas,
+}: {
+  areas: Area[];
+  personas: Persona[];
+}): JSX.Element {
   return (
     <div className="p-6">
       <section>
